a8881707062cd7896d8bdd4b5f4e6a336e1c2dab
Nicole Santamaria and Spencer Ruiz <spencer@>
2017-04-03 15:13:59 -0700

    add button to add new-address group to a contact. create address field variable that appends the fields to the form. create a reset form function to clear fields and hide additional address fields.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,92 @@
 //business logic
-function Contact(firstName, lastName, street, city, state){
+function Contact(firstName, lastName){
   this.firstName = firstName;
   this.lastName = lastName;
+  this.address = [];
+};
+
+function Address(street, city, state, zip) {
   this.street = street;
   this.city = city;
   this.state = state;
-}
+  this.zip = zip;
+};
 
 Contact.prototype.fullName = function() {
   return `${this.firstName} ${this.lastName}`;
 };
 
-Contact.prototype.fullAddress = function() {
-  return `${this.street}, ${this.city}, ${this.state}`;
+Address.prototype.fullAddress = function() {
+  return `${this.street}, ${this.city}, ${this.state} ${this.zip}`;
+};
+var addressFields = function(){
+  $("#address-group").append(`<div class="new-address">
+                                <div class="form-group">
+                                  <label for="street">Street</label>
+                                  <input class="form-control street-input" type="text">
+                                </div>
+                                <div class="form-group">
+                                  <label for="city">City</label>
+                                  <input class="form-control city-input" type="text">
+                                </div>
+                                <div class="form-group">
+                                  <label for="state">State</label>
+                                  <input class="form-control state-input" type="text">
+                                </div>
+                                <div class="form-group">
+                                  <label for="zipCode">Zip Code</label>
+                                  <input class="form-control zipCode-input" type="text">
+                                </div>
+                              </div>`);
 };
 
+var resetForm = function(){
+  $("#first-name-input").val("");
+  $("#last-name-input").val("");
+  $("input.street-input").val("");
+  $("input.city-input").val("");
+  $("input.state-input").val("");
+  $("input.zipCode-input").val("");
+  $("#address-group").empty();
+  addressFields();
+};
 //user interface logic
 $(document).ready(function(){
 
+  $("#new-address-button").click(function(){
+    addressFields();
+    });
   $("form#contact-input").submit(function(event){
     event.preventDefault();
-    var firstName = $("#first-name-input").val();
-    var lastName = $("#last-name-input").val();
-    var street = $("#street-input").val();
-    var city = $("#city-input").val();
-    var state = $("#state-input").val();
-    let contact = new Contact(firstName, lastName, street, city, state);
-    $("#contact-list").append("<li>" + contact.firstName + " " + contact.lastName + "</li>")
+    let firstName = $("#first-name-input").val();
+    let lastName = $("#last-name-input").val();
 
-    $("#contact-list").last().click(function(){
-      $(".contact-info").show();
-      $("#fullNameDisplay").text(contact.fullName());
-      $("#firstNameDisplay").text(contact.firstName);
-      $("#lastNameDisplay").text(contact.lastName);
-      $("#addressDisplay").empty();
-      $("#addressDisplay").append(`<li>${contact.fullAddress()}</li>`)
+    let newContact = new Contact(firstName, lastName);
+
+    $(".new-address").each(function(){
+      let street = $(this).find("input.street-input").val();
+      let city = $(this).find("input.city-input").val();
+      let state = $(this).find("input.state-input").val();
+      let zipCode = $(this).find("input.zipCode-input").val();
+
+      let newAddress = new Address(street, city, state, zipCode);
+
+      newContact.address.push(newAddress);
     });
 
+
+
+    $("#contact-list").append(`<li><span class="contactList">${newContact.fullName()}</li>`)
+
+    $(".contactList").last().click(function(){
+      $(".contact-info").show();
+      $("#fullNameDisplay").text(newContact.fullName());
+      $("#addressDisplay").empty();
+      newContact.address.forEach(function(address){
+        $("#addressDisplay").append(`<li>${address.fullAddress()}</li>`);
+      });
+    });
+    resetForm();
   });
 
 
